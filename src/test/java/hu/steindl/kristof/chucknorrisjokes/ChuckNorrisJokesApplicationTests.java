3ab7c3a87ad4491d4e3b2bd4574688adaafd30b2
Steindl Kristof <kristof.steindl@gmail.com>
2020-05-10 14:31:51 +0200

    initial commit

---
+++ src/test/java/hu/steindl/kristof/chucknorrisjokes/ChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.steindl.kristof.chucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
